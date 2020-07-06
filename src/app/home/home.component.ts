7be7659cf2a94c96372ce1e89a8d93ab4e21d233
Ace Zhang <tetra.oculus@gmail.com>
2020-07-05 22:18:25 -0400

    stylization added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { Ng2ImgMaxService } from 'ng2-img-max';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  status = ["finished a new project", "stated a new project", "got my supplies today!", "got accepted to an art school!", "gonna start working on my project soon!"]
+  
   constructor(
     private http: HomeHttpService,
     private ng2ImgMax: Ng2ImgMaxService
