357227d5121060aab4210bd1efda6972f294ca71
Ace Zhang <tetra.oculus@gmail.com>
2020-07-20 22:43:57 -0400

    Smart gallery resizing

--- src/app/galleries/galleries.component.ts
+++ src/app/galleries/galleries.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
 import { GalleriesHttpService } from './galleries-http.service';
-import { Gallery } from '../objects/gallery';
-import { stringify } from 'querystring';
-import { pipe, of, from, BehaviorSubject } from 'rxjs'
-import { map, flatMap, mergeMap } from 'rxjs/operators'
+
+const minWidth = 160
+const minHeight = 220
 
 @Component({
   selector: 'app-galleries',
   templateUrl: './galleries.component.html',
-  styleUrls: ['./galleries.component.css']
+  styleUrls: ['./galleries.component.css'],
+  styles: [
+    `
+      td {
+        border: 1px solid white;
+        min-width: ${minWidth};
+        min-height: ${minHeight};
+        object-fit: contain;
+      }
+    `
+  ]
 })
 export class GalleriesComponent implements OnInit {
+  @ViewChildren('preview') prev_cells;
 
-  gallery: Gallery;
-  fileURStream = [];
-  subj = new BehaviorSubject<Gallery>(null);
+  
+
+  item = "assets/5.jpg";
+  column1 = [];
+  column2 = [];
+  column3 = ['assets/5.jpg'];
 
   constructor(private route: ActivatedRoute,
-              private http: GalleriesHttpService,
-              private sanitizer: DomSanitizer) { }
-
+              private http: GalleriesHttpService) { }
+        
 
   ngOnInit(): void {    
     `
@@ -41,5 +52,16 @@ export class GalleriesComponent implements OnInit {
       } 
     );
     `
+  }
+  
+  onLoad(preview: any) {
+    preview.style.overflow = "hidden"
+    let ratio = minHeight/minWidth
+    if((preview.height/preview.width) > ratio){
+      preview.style.width = '160px';
+    }else{
+      preview.style.height = '220px';
+    }
+
   }
 }
