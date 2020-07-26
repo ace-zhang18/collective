7cb7835f33118ab2455b4a9e2dc65de4914f56d0
Ace Zhang <tetra.oculus@gmail.com>
2020-07-25 23:05:00 -0400

    images and gallery

--- src/app/galleries/galleries.component.ts
+++ src/app/galleries/galleries.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { GalleriesHttpService } from './galleries-http.service';
 
 const minWidth = 160
-const minHeight = 220
+const minHeight = 0
 let pageHeight = 2000
 
 @Component({
@@ -25,11 +25,22 @@ export class GalleriesComponent implements OnInit {
   @ViewChild('gallery') gallery;
 
   pageHeight = window.innerHeight;
+  extension = 400
 
   title = "SAMPLE TITLE"
 
-  item = ["assets/1.jpg", "assets/2.jpg", "assets/3.jpg", "assets/4.jpg", "assets/5.jpg", "assets/6.jpg", "assets/7.jpg",
-  "assets/8.jpg", "assets/9.jpg", "assets/10.jpg", "assets/11.jpg", "assets/12.jpg", "assets/13.jpg", "assets/14.jpg"];
+  item = [
+  "assets/1.jpg", "assets/2.jpg", "assets/3.jpg", "assets/4.jpg", "assets/5.jpg",
+  "assets/6.jpg", "assets/7.jpg", "assets/8.jpg", "assets/9.jpg", "assets/10.jpg",
+  "assets/11.jpg", "assets/12.jpg", "assets/13.jpg", "assets/14.jpg", "assets/15.jpg",
+  "assets/16.jpg", "assets/17.jpg", "assets/18.jpg", "assets/19.jpg", "assets/20.jpg",
+  "assets/21.jpg", "assets/22.jpg", "assets/23.jpg", "assets/24.jpg", "assets/25.jpg",
+  "assets/26.jpg", "assets/27.jpg", "assets/28.jpg", "assets/29.jpg", "assets/30.jpg",
+  "assets/31.jpg", "assets/32.jpg", "assets/33.jpg", "assets/34.jpg", "assets/35.jpg",
+  "assets/36.jpg", "assets/37.jpg", "assets/38.jpg", "assets/39.jpg", "assets/40.jpg",
+  "assets/41.jpg", "assets/42.jpg", "assets/43.jpg", "assets/44.jpg", "assets/45.jpg",
+  "assets/46.jpg", "assets/47.jpg", "assets/48.jpg"
+  ];
   column1 = [];
   height1 = 0;
   column2 = [];
@@ -49,6 +60,7 @@ export class GalleriesComponent implements OnInit {
 
   ngOnInit(): void {    
     this.transfer();
+    console.log(pageHeight);
     `
     of(this.route.snapshot.paramMap.get('term'))
     .pipe(
@@ -74,7 +86,7 @@ export class GalleriesComponent implements OnInit {
     if((preview.height/preview.width) > ratio){
       preview.style.width = '160px';
     }else{
-      preview.style.height = '220px';
+      //preview.style.height = '220px';
     }
     switch(index){
       case 1:
@@ -87,7 +99,9 @@ export class GalleriesComponent implements OnInit {
         this.height3 += preview.offsetHeight
         break;
     }
-    this.transfer()
+    if(this.height1 < this.pageHeight && this.height2 < this.pageHeight && this.height3 < this.pageHeight){
+      this.transfer()
+    }
   }
 
   transfer(){
@@ -104,7 +118,8 @@ export class GalleriesComponent implements OnInit {
   }
 
   onScrollDown() {
-    console.log('scrolled down!!');
+    this.pageHeight += this.extension;
+    this.transfer()
   }
  
   onScrollUp() {
