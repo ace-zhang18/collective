2e356e67f7123bdc66c0d15c29fa2483fae203f6
ace-zhang18 <tetra.oculus@gmail.com>
2020-09-22 11:26:31 -0400

    Revert "finished frontpage"
    This reverts commit 17f9051f5c35c7b27b195a17a743ab4cd447e2c7.

--- src/app/utils/expand-and-contract.animation.ts
+++ src/app/utils/expand-and-contract.animation.ts
@@ -3,9 +3,11 @@ import { style, trigger, state, transition, animate } from "@angular/animations"
 export const ExpandAndContractAnimation = (height, grow) => {
     return trigger(`ExpandAndContractAnimation${height}by${grow}`, [
         state('0', style({
+            overflow: 'hidden',
             height: `${height}vh`
         })),
         state('1', style({
+            overflow: 'hidden',
             height: `${height + grow}vh`
         })),
         transition('1 => 0', animate('1000ms ease-in-out')),
--- src/app/utils/widen-and-shrink.animation.ts
+++ src/app/utils/widen-and-shrink.animation.ts
@@ -3,9 +3,11 @@ import { style, trigger, state, transition, animate } from "@angular/animations"
 export const WidenAndShrinkAnimation = (width, grow) => {
     return trigger(`WidenAndShrinkAnimation${width}by${grow}`, [
         state('0', style({
+            overflow: 'hidden',
             width: `${width}vw`
         })),
         state('1', style({
+            overflow: 'hidden',
             width: `${width + grow}vw`
         })),
         transition('1 => 0', animate('1000ms ease-in-out')),
